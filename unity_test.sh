0c33bef397db25647f077ba25045569143ab81f5
esarkiss <edwin.sarkissian@jpl.nasa.gov>
2022-03-09 16:45:25 -0800

    a samll change

--- unity_test.sh
+++ unity_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello there!"
+echo "Hello there!  Today is a very nice day."
 echo "Jenkins is building Unity test project."
 echo "Current date/time is `date`"
 echo "End of build process! Have a nice day."
